0a99089e445f1c97197defd331caf64e52bacfeb
Benyamin <b.radmard77@gmai.com>
2019-10-07 06:30:20 +0200

    +Redesign EditOrAddScreen +Reposition floatingActionButton +Add dueDate field +Add Priority field +Add Project field

--- lib/AddOrEditScreen.dart
+++ lib/AddOrEditScreen.dart
@@ -10,6 +10,7 @@ import 'package:todoist_clone/db.dart';
 class AddOrEditScreen extends StatelessWidget{
   final TextEditingController _textEditingController = new TextEditingController();
   final DB _db = new DB();
+
   final String docID;
 
   AddOrEditScreen({this.docID});
@@ -18,14 +19,41 @@ class AddOrEditScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     final FirebaseUser firebaseUser = Provider.of<FirebaseUser>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: docID != null ? Text("Edit") : Text("Add"),
+      appBar:AppBar(
+        bottom: PreferredSize(
+            child: Padding(
+              padding: EdgeInsets.only(left: 200,right: 200,bottom: 50),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Task",
+                    style: new TextStyle(
+                    fontSize:18
+                    ),
+                  ),
+                  TextField(
+                    style: new TextStyle(
+                      fontSize: 50
+                    ),
+                    decoration: InputDecoration(
+                        hintText: "Add a task",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            preferredSize: Size.fromHeight(175)),
       ),
       body: docID != null ? StreamProvider<Task>.value(value: DB().getTask(docID, firebaseUser.uid),child: AddOrEditBody(_textEditingController),) : AddOrEditBody(_textEditingController),
       floatingActionButton: Builder(builder: (context){
-        return FloatingActionButton(
-          child: Icon(Icons.send),
-          onPressed:()=> docID != null ? _edit(context) : _add(context),
+        return Align(
+          alignment: Alignment.lerp(Alignment.centerRight, Alignment.topRight, 0.35),
+          child: FloatingActionButton(
+            backgroundColor: Colors.orange,
+            child: Icon(Icons.send),
+            onPressed:()=> docID != null ? _edit(context) : _add(context),
+          ),
         );
       })
     );
@@ -73,20 +101,42 @@ class AddOrEditBody extends StatelessWidget{
     //before assigning any data we need to check whether we have
     //task because this widget works with edit and add
     final Task task = Provider.of<Task>(context);
-
     if(task != null){
       _textEditingController.text = task.task;
     }
-
-    return Column(
-      children: <Widget>[
-        TextField(
-          controller: _textEditingController,
-          decoration: InputDecoration(
-            hintText: 'Enter your task...'
+    return Padding(
+      padding: const EdgeInsets.only(left:130 ),
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 8,
           ),
-        )
-      ],
+          //project
+          ListTile(
+            title: Text("Project"),
+            subtitle: Text("project"),
+            leading: Icon(Icons.assignment),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          //due date
+          ListTile(
+            title: Text("Due date"),
+            subtitle: Text("08.10.2023"),
+            leading: Icon(Icons.calendar_today),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          //priority
+          ListTile(
+            title: Text("Priority"),
+            subtitle: Text("priority 2"),
+            leading: Icon(Icons.outlined_flag,),
+          ),
+        ],
+      ),
     );
   }
 
